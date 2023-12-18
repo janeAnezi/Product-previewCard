67ad7a7490895a7ccc100cfdd50943922fbee1ef
Anezijane <ahnnieanezi@gmail.com>
2023-12-18 17:04:50 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  "content": ["./index.html"],
+  variants: {
+    extend: {
+      padding: ['hover'],
+    }
+  },
   theme: {
     fontFamily: {
       'sans': ['ui-sans-serif', 'system-ui'],
@@ -10,12 +15,20 @@ module.exports = {
       'body': ["Open Sans"]
     },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    extend: {},
-  },
-  plugins: [],
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  }
 }
